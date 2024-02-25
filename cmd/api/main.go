534d7699cfc9b2f642fe4781b42562a48d78cfd0
SantGT5 <gianspf@gmail.com>
2024-02-25 16:52:04 +0100

    api handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/SantGT5/goapi/internal/handlers"
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
@@ -11,4 +14,12 @@ func main() {
 
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handlers(r)
+
+	fmt.Println("Staring GO API service...")
+
+	err := http.ListenAndServe("localhost:8000", r)
+
+	if err != nil {
+		log.Error(err)
+	}
 }
