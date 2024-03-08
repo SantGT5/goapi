d7588bc43817dba81aa6c074a4cc0b6585ee91ac
SantGT5 <gianspf@gmail.com>
2024-03-08 21:14:51 +0100

    Coin balance API

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,1 +1,26 @@
 package handlers
+
+import (
+	"github.com/SantGT5/goapi/internal/middleware"
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+// By setting Handler with uppercase 'H'
+// this tells that this func can be imported to another packages
+func Handlers(r *chi.Mux) {
+	// StripSlashes will ensure that '/' wont be need
+	// at the end of the url api request
+	// example:
+	// /account/coins -> 200 - OK
+	// /account/coins/ -> 404 - OK
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		// Middleware for /account route
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinBalance)
+	})
+}
